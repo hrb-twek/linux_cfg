94d0779271c93d48247a1b18e67f8635d68eb9a8
hrb-twek <test@test.com>
2026-02-07 17:14:29 +0800

    添加文件

--- download.sh
+++ download.sh
@@ -1,3 +1,6 @@
 cp -f _bashrc ~/.bashrc
 cp -f vim/_vimrc ~/.vimrc
 cp -f vim/lnx/_gvimrc ~/.gvimrc
+cp -f _curlrc ~/.curlrc
+cp -f _gitconfig ~/.gitconfig
+sudo cp -f udev_rules/*.* /etc/udev/rules.d/
--- upload.sh
+++ upload.sh
@@ -1,3 +1,5 @@
 cp -f ~/.bashrc _bashrc
 cp -f ~/.vimrc vim/_vimrc 
 cp -f ~/.gvimrc vim/lnx/_gvimrc 
+cp -f ~/.gitconfig _gitconfig
+cp -f /etc/udev/rules.d/*.* ./udev_rules/
